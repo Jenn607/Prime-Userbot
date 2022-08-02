5ca6eb38a85ee4ef0aaf860e2ba6e575eea02286
Jenn607 <106703719+Jenn607@users.noreply.github.com>
2022-08-02 13:51:30 +0700

    Update makesession.py

--- makesession.py
+++ makesession.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 
-API_KEY = int(input("Enter API KEY: "))
-API_HASH = input("Enter API HASH: ")
+API_KEY = int(input("426fda5d-19cc-4c84-9f31-96a37abe3c65: "))
+API_HASH = input("f8db65e3150885246e6a641360cd2033: ")
 with Client(":memory:", api_id=API_KEY, api_hash=API_HASH) as app:
     print(app.export_session_string())
